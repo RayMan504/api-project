2afe14b79ef4f499f5a8a817e8f914edd44a9727
Uraeus Ray Millet <uraeusmillet@gmail.com>
2017-05-04 19:27:43 +0000

    discover /:query and req.params.query

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+// app.get('/', function (req, res) {
+//   res.send('Hello World!')
+// });
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
+});
+
+// pass in unix timestamp or language date
+app.get('/:query', function(req, res) {
+    console.log(req.params.query);
+    var date = req.params.query;
+    // if string has both timestamp and date 
+        // return both
+    // else
+        // return null
+    res.send('null');
 })
